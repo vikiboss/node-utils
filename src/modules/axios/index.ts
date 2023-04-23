1668bb337b2c3b18b74ea6fdf8be51ed4a66174a
Viki <hi@viki.moe>
2023-04-23 11:23:47 +0800

    chore: config axios

--- src/modules/axios/index.ts
+++ src/modules/axios/index.ts
@@ -8,8 +8,14 @@ export const ChromeUA = 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) Chrome/112.0.
 export const ChromeMobileUA = 'Mozilla/5.0 (Linux; Android 12; Mi 10) Chrome/112.0.0.0 Mobile'
 
 export const request = axios.create({
-  headers: { 'User-Agent': ChromeUA },
+  headers: {
+    'User-Agent': ChromeUA,
+    common: {
+      'Content-Type': 'application/json; charset=utf-8'
+    }
+  },
   timeout: 6000,
+  withCredentials: true,
   validateStatus: () => true,
   httpsAgent: new https.Agent({
     rejectUnauthorized: false
